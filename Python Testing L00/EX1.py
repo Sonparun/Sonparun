d6f19b59899129d90fae4c0b85d2da92f65c1f29
Sonparun <s6506022410023@email.kmutnb.ac.th>
2022-08-04 11:08:44 +0700

    ExPY1

---
+++ Python Testing L00/EX1.py
@@ -0,0 +1,9 @@
+s1=int(input('First test Score: '))
+s2=int(input('Second test Score: '))
+s3=int(input('Third test Score: '))
+
+s4=(s1+s2+s3)/3
+
+if s4>95:
+    print('Congratulate the user')
+print(s4)
---
+++ Python Testing L00/sale.py
@@ -0,0 +1,6 @@
+s=int(input("Your sales :"))
+if s>50000:
+    b=500
+    cr=0.12
+    sx=(s+b)
+    print('Youmet your sales quota!')
