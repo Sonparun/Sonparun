704ccedb76a090e2bbae6197907a1b74a5ec6b64
Sonparun <s6506022410023@email.kmutnb.ac.th>
2022-09-06 14:40:00 +0700

    ads

---
+++ Python Testing L00/Display_file.py
@@ -0,0 +1,13 @@
+from fileinput import filename
+
+
+def main():
+    filename=input('Enter a filename: ')
+    try:
+        infile=open(filename,'r')
+        contents=infile.read()
+        print(contents)
+        infile.close()
+    except IOError:
+        print('An error occurred trying to read the file',filename)
+main()
