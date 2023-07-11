b0e8f9b9199c3313d268537c062ad0482321bc02
Sonparun <s6506022410023@email.kmutnb.ac.th>
2023-07-11 11:12:04 +0700

    move in

---
+++ Java/LT02/or.js
@@ -0,0 +1,2 @@
+const status=500;
+if (status===200) {console.log('KO!');}else if (status===400 || status ===500) {console.log('Error!');}else {console.log('Unknown status');}
---
+++ Java/LT02/var-func.js
@@ -0,0 +1,4 @@
+let plushfive=(number) =>{return number +5;};
+let f =plushfive;
+console.log(plushfive(3));
+console.log(f(9));
