0a2e0191c415f29ff301f6469ad93729cc9fbdf7
Sonparun <s6506022410023@email.kmutnb.ac.th>
2023-07-11 10:19:00 +0700

    move

---
+++ Java/LT02/nummath.js
@@ -0,0 +1,11 @@
+let num1=100;
+console.log('**Basic Math**');
+console.log(num1+25);
+console.log(num1-100);
+console.log(num1*100);
+console.log(num1/1500);
+console.log(num1%1500);
+console.log(++num1);
+console.log(--num1);
+console.log(Math.PI);
+console.log(Math.sqrt(num1));
