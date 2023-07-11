4fd31c21b89de70bf4a47d4a7fd00bff2b690708
Sonparun <s6506022410023@email.kmutnb.ac.th>
2023-07-11 11:32:24 +0700

    sd

---
+++ Java/LT02/callbackfuns.js
@@ -0,0 +1,3 @@
+function creatreQuote(quote,callback) {let myQuote ='Like I always say, '+ quote;callback(myQuote);}
+function logQuote(quote) {console.log(quote + ' Yes..');}
+creatreQuote('you will getting better!',logQuote);
