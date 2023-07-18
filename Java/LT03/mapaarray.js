48044179e3ace4fcb443a0c793d6ad0c47758ab5
Sonparun <s6506022410023@email.kmutnb.ac.th>
2023-07-18 09:04:50 +0700

    jh

---
+++ Java/LT03/mapaarray.js
@@ -0,0 +1,3 @@
+const finalPartipants=['Taylor','Donald','Don','Natasha','Bobby'];
+const announcement=finalPartipants.map((member)=>{return member+' joined the contest.';});
+console.log(announcement)
