e4561c7e48214525d0960566533ea823d30204ce
Sonparun <s6506022410023@email.kmutnb.ac.th>
2023-07-18 10:24:53 +0700

    df

---
+++ Java/LT03/cloneObj.js
@@ -0,0 +1,5 @@
+const obj1={person:'A',weight:55}
+const obj2={... obj1}
+obj2.weight=65;
+console.log(obj1);
+console.log(obj2);
---
+++ Java/LT03/forofloop.js
@@ -0,0 +1,2 @@
+const names=['Justin','Sarah','Christopher'];
+for (let name of names){console.log(name);}
